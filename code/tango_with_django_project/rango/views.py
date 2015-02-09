cf1a736311ecf1d788d449c5e8c41b9900051511
Vincent Wong <0706209w>
2015-02-09 06:41:36 +0000

    chapter7 update

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -37,7 +37,8 @@ def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    pages_list = Page.objects.order_by('views')[:5]
+    context_dict = {'categories': category_list, 'pages': pages_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -49,7 +50,7 @@ def index(request):
 def about(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "about"}
+    context_dict = {'boldmessage': ""}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
