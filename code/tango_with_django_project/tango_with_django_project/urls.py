15654c428763d0884fb8f191ed771fe43e2d6f8e
Vincent Wong <0706209w>
2015-02-09 05:34:23 +0000

    Chapter 7

--- code/tango_with_django_project/tango_with_django_project/urls.py
+++ code/tango_with_django_project/tango_with_django_project/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from rango import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,8 +9,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
-)
+    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
+    url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)  # New!
 
 if settings.DEBUG:
     urlpatterns += patterns(
