4835c155a6dce2574b942312bb0ba73a14777d09
Vincent Wong <0706209w>
2015-02-09 08:35:35 +0000

    chapter 8 complete

--- code/tango_with_django_project/tango_with_django_project/urls.py
+++ code/tango_with_django_project/tango_with_django_project/urls.py
@@ -9,7 +9,9 @@ urlpatterns = patterns('',
     url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^rango/category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)  # New!
+    url(r'^rango/category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+    url(r'^rango/add_category/$', views.add_category, name='add_category'), # New!
+    url(r'^rango/category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),)  # New!
 
 if settings.DEBUG:
     urlpatterns += patterns(
