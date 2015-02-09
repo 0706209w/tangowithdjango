3601c8269e93f8e7ee241c5a5394dfd537288590
Vincent Wong <0706209w>
2015-02-09 06:02:15 +0000

    chapter7

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -49,7 +49,7 @@ def index(request):
 def about(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': ""}
+    context_dict = {'boldmessage': "about"}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
