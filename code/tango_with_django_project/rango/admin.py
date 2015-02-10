6d8460b49374ab9b4e158598d06ba6b049e0bef4
Vincent Wong <0706209w>
2015-02-10 19:03:03 +0000

    Chapter 9 complete

--- code/tango_with_django_project/rango/admin.py
+++ code/tango_with_django_project/rango/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 # Register your models here.
+admin.site.register(UserProfile)
 
 # Add in this class to customized the Admin Interface
 class CategoryAdmin(admin.ModelAdmin):
--- code/tango_with_django_project/tango_with_django_project/urls.py
+++ code/tango_with_django_project/tango_with_django_project/urls.py
@@ -5,13 +5,18 @@ from rango import views
 
 urlpatterns = patterns('',
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
-    url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about'),
-    url(r'^rango/category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
-    url(r'^rango/add_category/$', views.add_category, name='add_category'), # New!
-    url(r'^rango/category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),)  # New!
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^rango/', include('rango.urls')), 
+	url(r'^$', views.index, name='index'),
+	url(r'^about/$', views.about, name='about'),
+	url(r'^rango/category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+	url(r'^rango/add_category/$', views.add_category, name='add_category'), 
+	url(r'^rango/category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+	url(r'^rango/register/$', views.register, name='register'),
+	url(r'^rango/login/$', views.user_login, name='login'),
+	url(r'^rango/restricted/', views.restricted, name='restricted'),
+	url(r'^rango/logout/$', views.user_logout, name='logout'),
+	)  # New!
 
 if settings.DEBUG:
     urlpatterns += patterns(
